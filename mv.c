4d365777e710d28b96a8967ad8a1de6683d9231a
unknown <studyadailo@gmail.com>
2023-04-13 20:20:57 -0300

    Update mv.c

--- mv.c
+++ mv.c
@@ -5,21 +5,29 @@
 
 int main(){
     int escolha;
-    float Pr, Po=100, x;
+    float Pr, Po=100, x, No=1, Mr, Mo;
     printf("Calculo de meia vida de uma materia radioativa!\n");
     printf("Como voce deseja calcular a meia vida? \n[1] Porcentagem \n[2] Fracao \n[3] Massa \nEscolha: ");
     scanf("%d", &escolha);
     if (escolha == 1){
-        printf("Para descobrir a porcentagem do material radioativo que resta na atmosfera, digite o numero de meias-vidas que passaram: ");
+        printf("Para descobrir a porcentagem do material radioativo que resta na amostra, digite o numero de meias-vidas que passaram: ");
         scanf("%f", &x);
         Pr = Po / pow(2, x);
         printf("Se passaram %.0f meias vidas, entao ainda resta %.3f por cento do material", x, Pr);
         } else if (escolha == 2) {
-        printf("Faca isso");
-    } /* else if (escolha == 3) {
-        printf("Faca isso");
+        printf("Para descobrir a fracao referente ao material radioativo que resta na amostra, digite o numero de meias-vidas que passaram: ");
+        scanf("%f", &x);
+        printf("A fracao referente ao material que resta na amostra e: %.0f / %.0f", No, pow(2, x));
+    } else if (escolha == 3) {
+        printf("Para saber a massa do material radioativo que resta na amostra, digite a Massa inicial da amostra: ");
+        scanf("%f", &Mo);
+        printf("Agora digite o numero de meias-vidas");
+        scanf("%f", &x);
+        Mr = Mo / pow(2, x);
+        printf("A massa restante do material radioativo e: %.2f", Mr);
     } else {
         printf("Opcao invalida, tente novamente! \n\n");
         sleep(1.5);
-        main();*/
+        main();
     }
+}
