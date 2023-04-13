2f1ba20d84e55b03afc0ee90bb9294b6310a1510
unknown <studyadailo@gmail.com>
2023-04-13 20:39:57 -0300

    Complex delta value

--- eq2g.c
+++ eq2g.c
@@ -6,7 +6,7 @@ int main(){
 	
 	//Declarando as variaveis
 	
-	int a, b, c, delta, rdelta, x1, x2;
+	int a, b, c, delta, rdelta, x1, x2, ix1, ix2;
 
 	printf("------------------------------\n");
 	printf("Equacao do 2 grau!\n");
@@ -26,10 +26,11 @@ int main(){
 	delta = pow(b, 2) - (4 * a * c); //b�-4ac
 	
 	//Calculando os X's
-	rdelta = sqrt(delta);	
+		
 	
 	if (a != 0) {
 		if (delta >= 0){
+			rdelta = sqrt(delta);
 			x1 = ((b * -1) + rdelta) / (2*a); //-b +/- raiz do delta / 2a
 			x2 = ((b * -1) - rdelta) / (2*a);
 			printf("Delta: %d \nX1: %d \nX2: %d \n", rdelta, x1, x2);
@@ -40,7 +41,13 @@ int main(){
 			}
 		} else {
 			printf("Nao existe raiz real, fazer com numeros complexos\n");
-			//Continua
+			delta *= -1;
+			rdelta = sqrt(delta); //lembrar de colocar o i
+			x1 = -1 * b / (2*a);
+			ix1 = rdelta / 2*a;
+			x2 = -1 * b / (2*a);
+			ix2 = rdelta / 2*a;
+			printf("S: {%d + %di, %d - %di}", x1, ix1, x2, ix2);
 		}
 	} else {
 		printf("Equacoes do segundo grau so existem se o valor de (a) for diferente de zero");
